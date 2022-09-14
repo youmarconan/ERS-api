7bff1ca6c4c0c28f7023f773ce35c42300bf1cd5
Marco Younan <youmarconan@gmail.com>
2022-09-14 13:57:29 -0700

    updated sql scripts

--- project-one/src/main/resources/p1 insertion script.sql
+++ project-one/src/main/resources/p1 insertion script.sql
@@ -1,5 +1,7 @@
 insert into reimbursement_status ("name") values ('pending'),('approved'),('denied');
 
+select * from reimbursement_status;
+
 insert into reimbursement_type ("name") values ('lodging'),('travel'),('food'),('other');
 
 insert into user_role ("name") values ('admin'),('manager'),('employee');
@@ -13,6 +15,7 @@ values ('youmarco','marcoyounan@gmail','p@$$word','marco','younan',true,'1');
 select * from "user";
 
 select "user".username as username,
+"user".id  as user_id,
 "user".email as email,
 "user"."password" as "password",
 "user".first_name as first_name,
@@ -24,7 +27,39 @@ from "user"
 join user_role
 on user_role.id = "user".role_id;
 
+insert into reimbursement (amount,description,author_id,type_id)
+values (2000.35,'description','13','2');
+
+insert into reimbursement (amount,description,author_id,type_id)
+values (1068.35,'description','12','4');
+
+select * 
+from reimbursement 
+join "user"
+on "user".id = reimbursement.author_id;
+
+select reimbursement.id as reimbursement_id,
+reimbursement.amount as amount,
+reimbursement.submitted as submitted,
+reimbursement.resolved as resolved,
+reimbursement.description as description,
+reimbursement.author_id as author_id,
+reimbursement.resolver_id as resolver_id,
+reimbursement_type."name" as "type",
+reimbursement_status."name" as status
+from reimbursement
+join reimbursement_type
+on reimbursement_type.id = reimbursement.type_id 
+join reimbursement_status
+on reimbursement_status.id = reimbursement.status_id ;
+
+
+update reimbursement 
+set amount  = '9999.99',
+type_id  = 1,
+description  = 'this is new description'
+where reimbursement.id =3 ;
 
 
 
-select "user".username as username, "user".email as email, "user"."password" as "password", "user".first_name as first_name, "user".last_name as last_name, "user".is_active as is_active, user_role.id as role_id, user_role.name as role_name from "user" join user_role on user_role.id = "user".role_id where username = 'youmarco' and "password" = 'p@$$word' ;
+
--- project-one/src/main/resources/p1 tables creation script.sql
+++ project-one/src/main/resources/p1 tables creation script.sql
@@ -44,13 +44,13 @@ primary key (id)
 create table reimbursement(
 id serial,
 amount numeric(6,2) not null,
-submitted timestamp not null,
-resolved timestamp,
+submitted timestamp default CURRENT_TIMESTAMP,
+resolved timestamp ,
 description varchar,
-author_id serial,
-resolver_id serial,
-status_id serial,
-type_id serial,
+author_id serial not null,
+resolver_id int default null,
+status_id int default 1,
+type_id int default 1, 
 
 foreign key (author_id) references "user" (id),
 foreign key (resolver_id) references "user" (id),
@@ -59,6 +59,6 @@ foreign key (type_id) references reimbursement_type (id),
 primary key (id)
 );
 
-
+drop table reimbursement;
 
 
