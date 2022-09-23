e3f59d06127dd335d84f28364246f1a4407886c0
Marco Younan <youmarconan@gmail.com>
2022-09-23 14:01:34 -0700

    updated updateRequestBody

--- src/main/java/com/revature/users/UpdateRequestBody.java
+++ src/main/java/com/revature/users/UpdateRequestBody.java
@@ -1,44 +1,113 @@
 package com.revature.users;
 
+import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class UpdateRequestBody {
 
-    private String updateTo;
-    private String userId;
+    private UUID userId;
+    private String username;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    @JsonProperty("isActive")
+    private boolean isActive;
+    private UUID userRoleId;
 
-    
     public UpdateRequestBody() {
         super();
     }
 
-    
-    public UpdateRequestBody(String updateTo, String userId) {
-        this.updateTo = updateTo;
+    public UpdateRequestBody(UUID userId, String username, String email, String password, String firstName,
+            String lastName, boolean isActive, UUID userRoleId) {
         this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.isActive = isActive;
+        this.userRoleId = userRoleId;
     }
 
-
-    public String getUpdateTo() {
-        return updateTo;
-    }
-
-    public void setUpdateTo(String updateTo) {
-        this.updateTo = updateTo;
-    }
-
-    public String getUserId() {
+    public UUID getUserId() {
         return userId;
     }
 
-    public void setUserId(String userId) {
+    public void setUserId(UUID userId) {
         this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public UUID getUserRoleId() {
+        return userRoleId;
+    }
+
+    public void setUserRoleId(UUID userRoleId) {
+        this.userRoleId = userRoleId;
     }
 
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((updateTo == null) ? 0 : updateTo.hashCode());
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + (isActive ? 1231 : 1237);
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
         result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+        result = prime * result + ((userRoleId == null) ? 0 : userRoleId.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
         return result;
     }
 
@@ -51,22 +120,53 @@ public class UpdateRequestBody {
         if (getClass() != obj.getClass())
             return false;
         UpdateRequestBody other = (UpdateRequestBody) obj;
-        if (updateTo == null) {
-            if (other.updateTo != null)
+        if (email == null) {
+            if (other.email != null)
                 return false;
-        } else if (!updateTo.equals(other.updateTo))
+        } else if (!email.equals(other.email))
+            return false;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (isActive != other.isActive)
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
             return false;
         if (userId == null) {
             if (other.userId != null)
                 return false;
         } else if (!userId.equals(other.userId))
             return false;
+        if (userRoleId == null) {
+            if (other.userRoleId != null)
+                return false;
+        } else if (!userRoleId.equals(other.userRoleId))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return "UpdateRequestBody [updateTo=" + updateTo + ", userId=" + userId + "]";
+        return "UpdateRequestBody [email=" + email + ", firstName=" + firstName + ", isActive=" + isActive
+                + ", lastName=" + lastName + ", password=" + password + ", userId=" + userId + ", userRoleId="
+                + userRoleId + ", username=" + username + "]";
     }
+   
+    
 
 }
