90537b70ff0ff3093ff58c77701cc865bf7fcc8e
Marco Younan <youmarconan@gmail.com>
2022-09-23 14:03:01 -0700

    created the userRepo to replace the DAO

---
+++ src/main/java/com/revature/users/UserRepo.java
@@ -0,0 +1,22 @@
+package com.revature.users;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, UUID> {
+    
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    Optional<User> findUserByUsernameAndPassword(String username, String password);
+
+    Optional<User> findUserByUsername(String username);
+
+    Optional<User> findUserByEmail(String email);
+
+}
