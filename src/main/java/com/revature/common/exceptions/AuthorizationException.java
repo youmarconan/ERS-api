66b41c2e53155e9e49ff02a1436acd348ef4896d
Marco Younan <youmarconan@gmail.com>
2022-09-23 22:35:26 -0700

    Created new exception for authorizations

---
+++ src/main/java/com/revature/common/exceptions/AuthorizationException.java
@@ -0,0 +1,13 @@
+package com.revature.common.exceptions;
+
+public class AuthorizationException extends RuntimeException {
+    
+    public AuthorizationException() {
+        super("A illegal request was made to an endpoint");
+    }
+
+    public AuthorizationException(String message) {
+        super(message);
+    }
+    
+}
